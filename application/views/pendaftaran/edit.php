565544d0990e0ef82839ecc057a2ed170302be27
hudamiftahul <83411520+hudamiftahul@users.noreply.github.com>
2021-07-20 18:40:50 +0700

    Update data pendaftaran + nomor antrian otomatis

--- application/views/pendaftaran/edit.php
+++ application/views/pendaftaran/edit.php
@@ -7,7 +7,7 @@
             <div class="col-lg-9 col-sm-8 col-md-8 col-xs-12">
                 <ol class="breadcrumb">
                     <li><a href="#">Pendaftaran</a></li>
-                    <li class="active">Tambah</li>
+                    <li class="active">Ubah</li>
                 </ol>
             </div>
             <!-- /.col-lg-12 -->
@@ -19,72 +19,72 @@
                     <form class="form-horizontal" method="POST" action="">
                         <div class="form-group">
                             <div class="col-md-11">
-                                <h3 class="box-title mb-3 float-left">Tambah Pendaftaran</h3>
+                                <h3 class="box-title mb-3 float-left">Ubah Pendaftaran</h3>
                             </div>
                             <div class="col-md-1">
-                                <input type="text" readonly name="no_antrian" class="form-control float-right text-center bg-info text-white" value="<?= $no_antrian; ?>">
+                                <input type="text" name="no_antrian" class="form-control float-right text-center bg-info text-white" value="<?= $dftr['no_antrian']; ?>">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-group">
                                 <label class="col-md-12">Id Daftar</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan id daftar ..." name="id_dftr">
+                                    <input type="text" class="form-control" placeholder="Masukkan id daftar ..." name="id_dftr" value="<?= $dftr['id_dftr']; ?>">
                                     <i class="text-danger"><?= form_error('id_dftr'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Id Pasien</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan id pasien ..." name="id_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan id pasien ..." name="id_pasien" value="<?= $dftr['id_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('id_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Tanggal Daftar</label>
                                 <div class="col-md-12">
-                                    <input type="datetime-local" class="form-control" name="tgl_dftr">
+                                    <input type="datetime-local" class="form-control" name="tgl_dftr" value="<?= $tgl_dftr; ?>">
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Poli Tujuan</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan poli tujuan ..." name="poli_tujuan">
+                                    <input type="text" class="form-control" placeholder="Masukkan poli tujuan ..." name="poli_tujuan" value="<?= $dftr['poli_tujuan']; ?>">
                                     <i class="text-danger"><?= form_error('poli_tujuan'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">No RM</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan no RM ..." name="no_rm">
+                                    <input type="text" class="form-control" placeholder="Masukkan no RM ..." name="no_rm" value="<?= $dftr['no_rm']; ?>">
                                     <i class="text-danger"><?= form_error('no_rm'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Nama Pasien</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan nama pasien ..." name="nm_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan nama pasien ..." name="nm_pasien" value="<?= $dftr['nm_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('nm_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Tempat Lahir</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan tempat lahir ..." name="tempat_lahir">
+                                    <input type="text" class="form-control" placeholder="Masukkan tempat lahir ..." name="tempat_lahir" value="<?= $dftr['tempat_lahir']; ?>">
                                     <i class="text-danger"><?= form_error('tempat_lahir'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Tanggal Lahir</label>
                                 <div class="col-md-12">
-                                    <input type="date" class="form-control" placeholder="Masukkan tanggal lahir ..." name="tgl_lhr_pasien">
+                                    <input type="date" class="form-control" placeholder="Masukkan tanggal lahir ..." name="tgl_lhr_pasien" value="<?= $dftr['tgl_lhr_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('tgl_lhr_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Umur</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan umur ..." name="umur">
+                                    <input type="text" class="form-control" placeholder="Masukkan umur ..." name="umur" value="<?= $dftr['umur']; ?>">
                                     <i class="text-danger"><?= form_error('umur'); ?></i>
                                 </div>
                             </div>
@@ -95,15 +95,15 @@
                                 <div class="col-sm-12">
                                     <select class="form-control" name="jenis_pasien">
                                         <option readonly>-- Pilih Jenis Pasien --</option>
-                                        <option value="BPJS">BPJS</option>
-                                        <option value="KIS">KIS</option>
+                                        <option value="BPJS" <?= ($dftr['jenis_pasien'] == "BPJS") ? 'selected' : '' ?>>BPJS</option>
+                                        <option value="KIS" <?= ($dftr['jenis_pasien'] == "KIS") ? 'selected' : '' ?>>KIS</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Nama KK</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan nama KK ..." name="kk_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan nama KK ..." name="kk_pasien" value="<?= $dftr['kk_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('kk_pasien'); ?></i>
                                 </div>
                             </div>
@@ -112,43 +112,43 @@
                                 <div class="col-sm-12">
                                     <select class="form-control" name="jenis_kelamin">
                                         <option readonly>-- Pilih Jenis Kelamin --</option>
-                                        <option value="Laki-Laki">Laki-Laki</option>
-                                        <option value="Perempuan">Perempuan</option>
+                                        <option value="Laki-Laki" <?= ($dftr['j_kel_pasien'] == "Laki-Laki") ? 'selected' : '' ?>>Laki-Laki</option>
+                                        <option value="Perempuan" <?= ($dftr['j_kel_pasien'] == "Perempuan") ? 'selected' : '' ?>>Perempuan</option>
                                     </select>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Alamat</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan alamat ..." name="almt_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan alamat ..." name="almt_pasien" value="<?= $dftr['almt_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('almt_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Kota</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan kota ..." name="kota_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan kota ..." name="kota_pasien" value="<?= $dftr['kota_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('kota_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Kecamatan</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan kecamatan ..." name="kec_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan kecamatan ..." name="kec_pasien" value="<?= $dftr['kec_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('kec_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Desa</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan desa ..." name="desa_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan desa ..." name="desa_pasien" value="<?= $dftr['desa_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('desa_pasien'); ?></i>
                                 </div>
                             </div>
                             <div class="form-group">
                                 <label class="col-md-12">Pekerjaan</label>
                                 <div class="col-md-12">
-                                    <input type="text" class="form-control" placeholder="Masukkan pekerjaan ..." name="pkjr_pasien">
+                                    <input type="text" class="form-control" placeholder="Masukkan pekerjaan ..." name="pkjr_pasien" value="<?= $dftr['pkjr_pasien']; ?>">
                                     <i class="text-danger"><?= form_error('pkjr_pasien'); ?></i>
                                 </div>
                             </div>
